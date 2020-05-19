b3be946e45b7eb578c405d1ce57c1cbb56434412
Dominic McPhee <dominic.mcphee@shopify.com>
2020-05-19 14:44:08 -0400

    Adding default key claim token for development

--- app/controllers/keys_controller.rb
+++ app/controllers/keys_controller.rb
@@ -5,7 +5,12 @@ require 'json'
 class KeysController < ApplicationController
   def generate
     uri = URI.parse("https://"+ ENV['KEY_CLAIM_HOST'] +"/new-key-claim")
-    header = {'Authorization': "Bearer #{Rails.application.credentials.key_claim_token}" }
+    token = if Rails.env.production?
+      Rails.application.credentials.key_claim_token
+    else
+      'test'
+    end
+    header = {'Authorization': "Bearer #{token}" }
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = uri.scheme == 'https'
     request = Net::HTTP::Post.new(uri.request_uri, header)
