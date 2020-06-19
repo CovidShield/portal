b14042862ead9ba1827b1229d735e1107240fc78
Steven Scott <steven.scott@shopify.com>
2020-06-19 14:50:48 -0400

    prevent marking of last admin as staff

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @admin = users(:admin)
+    @staff = users(:staff)
+  end
+
+  test "cannot update the last admin to be staff" do
+    assert User.admin.count == 1
+    @admin.admin = false
+    refute @admin.save
+  end
+
+  test "can revert admin to staff if there is another admin" do
+    @staff.update(admin: true)
+    assert @admin.update(admin: false)
+  end
 end
